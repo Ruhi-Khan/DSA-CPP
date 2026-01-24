bef90b8959631fabccdfd1884023c9de1510b039
Your Name <khanruhi2630@gmail.com>
2026-01-24 12:58:40 +0530

    1877. Minimize Maximum Pair Sum in Array

---
+++ Day_8.cpp
@@ -0,0 +1,14 @@
+class Solution:
+    def minPairSum(self, nums):
+        nums.sort()
+        
+        i = 0
+        j = len(nums) - 1
+        ans = 0
+        
+        while i < j:
+            ans = max(ans, nums[i] + nums[j])
+            i += 1
+            j -= 1
+        
+        return ans
