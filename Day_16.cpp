dbc39e0ba0c1686c81dc0f2e5809997f0f050c67
Your Name <khanruhi2630@gmail.com>
2026-02-01 13:53:27 +0530

    3010. Divide an Array Into Subarrays With Minimum Cost I

---
+++ Day_16.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        int n = nums.size();
+        int ans = INT_MAX;
+
+        // choose i and j as starting points of 2nd and 3rd subarrays
+        for (int i = 1; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                ans = min(ans, nums[0] + nums[i] + nums[j]);
+            }
+        }
+
+        return ans;
+    }
+};
