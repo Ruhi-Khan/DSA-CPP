5fd738e62c6aa35047af0c64c014a7a32b85dcd7
Your Name <khanruhi2630@gmail.com>
2026-02-08 00:05:54 +0530

    1653. Minimum Deletions to Make String Balanced

---
+++ Day_18.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int bCount = 0;
+        int deletions = 0;
+
+        for (char c : s) {
+            if (c == 'b') {
+                bCount++;
+            } else { // 'a'
+                deletions = min(deletions + 1, bCount);
+            }
+        }
+        return deletions;
+    }
+};
